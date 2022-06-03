dc0e90e77df58dd331c9e618ae63a4a841efe5d6
metanouvelle <metanouvelle22@gmail.com>
2022-06-03 13:55:24 +0200

    refactor pipeline for multiple exporting

--- mondu_website_scrapper/pipelines.py
+++ mondu_website_scrapper/pipelines.py
@@ -5,12 +5,25 @@ import logging
 from scrapy.exporters import CsvItemExporter
 from mondu_website_scrapper.utils import extract_categories_from_wappalyzer
 
+from mondu_website_scrapper import items
+
+
+def item_type(item):
+    """
+    get and lower item class name
+    """
+    return type(item).__name__.lower()
+
 
 class MonduWebsiteScrapperPipeline:
     """
     this is the pipeline taking sccrapped items, processing items,
     and then exporting it into csv file.
     """
+
+    defined_items = [
+        name.lower() for name, _ in items.__dict__.items() if "Item" in name
+    ]
 
     def __init__(self, file_folder):
         self.file_folder = file_folder
@@ -26,34 +39,46 @@ class MonduWebsiteScrapperPipeline:
             file_folder=crawler.settings.get("FILE_FOLDE"),
         )
 
-    def open_spider(self, spider):
+    def open_spider(self, spider):  # pylint: disable=unused-argument
         """
         open file, define exporter and start exporting
         """
+        self.files = dict(
+            [
+                (name, open(self.file_folder / f"{name}.csv", "w+b"))
+                for name in self.defined_items
+            ]
+        )
+        self.exporters = dict(
+            [(name, CsvItemExporter(self.files[name])) for name in self.defined_items]
+        )
         logging.info("Starting exporting into csv...")
-        # with open(self.file_folder / f"{spider.name}.csv", "wb") as self.file:
-        self.file = open(self.file_folder / f"{spider.name}.csv", "wb")
-        self.exporter = CsvItemExporter(self.file)
-        self.exporter.start_exporting()
+
+        for exporter in self.exporters.values():
+            exporter.start_exporting()
+
+    def close_spider(self, spider):  # pylint: disable=unused-argument
+        """
+        finishining exporting and close the file
+        """
+        logging.info("Finishing exporting into csv...")
+        for exporter in self.exporters.values():
+            exporter.finish_exporting()
+        for file in self.files.values():
+            file.close()
 
     def process_item(self, item, spider):  # pylint: disable=unused-argument
-
         """
         process items scrapped from web.
         1. only the wappalyzer item needs to be processed. check function
         extract_categories_from_wappalyzer for more details
         """
-        item["wappalyzer"] = extract_categories_from_wappalyzer(item["wappalyzer"])
-        logging.info("Exporting items into csv...")
-        self.exporter.export_item(item)
-
+        item_name = item_type(item)
+        if item_name in set(self.defined_items):
+            if item_name == "generalinformationitem":
+                item["wappalyzer"] = extract_categories_from_wappalyzer(
+                    item["wappalyzer"]
+                )
+            logging.info("Exporting items into csv...")
+            self.exporters[item_name].export_item(item)
         return item
-
-    def close_spider(self, spider):  # pylint: disable=unused-argument
-
-        """
-        finishining exporting and close the file
-        """
-        logging.info("Finishing exporting into csv...")
-        self.exporter.finish_exporting()
-        self.file.close()
