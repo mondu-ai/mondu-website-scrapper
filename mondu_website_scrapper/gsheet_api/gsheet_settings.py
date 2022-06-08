4def7c2756d1b41173de61c458be6fc89b5f0fc2
metanouvelle <metanouvelle22@gmail.com>
2022-06-08 11:50:15 +0200

    clean file

--- mondu_website_scrapper/gsheet_api/gsheet_settings.py
+++ mondu_website_scrapper/gsheet_api/gsheet_settings.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 
+import os
+
 SCOPES = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive",
 ]
+
 
 CLIENT_SECRET_JSON = Path(__file__).parent / "client_secret.json"
 EXPORT_DATA_FOLDER = Path(__file__).parent / "scraped_results"
