a12271d86f360c1e5a9f20e4fdbae964b3094fcd
metanouvelle <metanouvelle22@gmail.com>
2022-06-08 12:35:58 +0200

    implement dotenv

--- mondu_website_scrapper/gsheet_api/export2_gsheet.py
+++ mondu_website_scrapper/gsheet_api/export2_gsheet.py
@@ -2,13 +2,14 @@ from datetime import datetime  # pylint: disable=missing-module-docstring
 from pathlib import Path
 from typing import Union
 
-from .gsheet_settings import (
+from mondu_website_scrapper.gsheet_api.gsheet_settings import (
     CLIENT_SECRET_JSON,
     EXPORT_DATA_FOLDER,
     SCOPES,
     SPREADSHEET_NAME,
 )
-from .utils import (
+
+from mondu_website_scrapper.gsheet_api.utils import (
     create_worksheet,
     get_gsheet_client,
     get_gsheet_credential,
@@ -32,7 +33,6 @@ def export_report(
     if spreadsheet_name is None:
         spreadsheet_name = SPREADSHEET_NAME
     client_secret = get_gsheet_credential(client_secret_file=CLIENT_SECRET_JSON)
-    print("client_secret", client_secret)
     gsheet_client = get_gsheet_client(client_secret=client_secret, scopes=SCOPES)
     now = datetime.now()
     dt_now = now.strftime("%Y/%m/%d %H:%M")
